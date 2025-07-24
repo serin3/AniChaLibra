0aeda63927c2c3bf0cc4fd2c246f0da3018e9b1f
NightcoreAT <oussamakatalonya1@gmail.com>
2025-07-24 19:38:10 +0100

    ✨| REWORKED

--- src/ar/animeCharacters/animeChDB.py
+++
@@ -1,7 +0,0 @@
-import requests as animeCharacterDB
-
-fetchInformation = animeCharacterDB.get('https://raw.githubusercontent.com/serin3/AniChaLibra/master/src/ar/animeCharacters/animeChDB.json')
-
-animechdb = fetchInformation.text
-
-print(animechdb)
