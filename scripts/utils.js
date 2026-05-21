01c8972bbd708dbd5febff1bc2f31ff1f8830dcf
NightcoreAT <oussamakatalonya1@gmail.com>
2026-05-21 23:27:20 +0100

    ✨| UPDATED

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,8 +2,8 @@ export function cleanString(str) {
   return str.toLowerCase().replace(/[^a-z0-9]/gi, '');
 }
 
-export function extractRelatives(relatives) {
-  return Object.entries(relatives)
+export function extractRelatives(relationships) {
+  return Object.entries(relationships)
     .filter(([key, val]) => val && val !== "null")
     .map(([key, val]) => `${key}: ${val}`);
 }
